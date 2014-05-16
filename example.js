3c8a991bb7fe59d665c452e1b316d35bd4421cbb
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-16 15:40:07 +0200

    use sha256 in example

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
+var proquint = require('./')
 var crypto = require('crypto')
 var fs = require('fs')
 
 var pubkey = fs.readFileSync(process.env.HOME + '/.ssh/id_rsa.pub')
-var hash = crypto.createHash('sha').update(pubkey).digest()
+var hash = crypto.createHash('sha256').update(pubkey).digest()
 
-var proquint = require('./')
 
 console.log(proquint.encode(hash))
+
 
